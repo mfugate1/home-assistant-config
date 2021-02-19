6c31636dec44b88ebff7033f0c875e95795364fe
Mike Fugate <mfugate@carnegielearning.com>
2021-02-19 11:06:30 -0500

    disable service calls until config parameter changes are implemented in zwavejs

--- python_scripts/inovelli_led.py
+++ python_scripts/inovelli_led.py
@@ -45,15 +45,15 @@ value = value + (effect * 16777216)
 if effect == 0:
     value = 0
     
-if 'node_ids' in data:
-    node_ids = data.get('node_ids')
-else:
-    node_ids = []
-    for entity in hass.states.entity_ids('zwave'):
-        attributes = hass.states.get(entity).attributes
-        if 'LZW31-SN' in attributes['product_name']:
-            node_ids.append(attributes['node_id'])
-            
-for node_id in node_ids:
-    service_data = {'node_id': node_id, 'parameter': 16, 'size': 4, 'value': int(value)}
-    hass.services.call('zwave', 'set_config_parameter', service_data, False)
+#if 'node_ids' in data:
+#    node_ids = data.get('node_ids')
+#else:
+#    node_ids = []
+#    for entity in hass.states.entity_ids('zwave'):
+#        attributes = hass.states.get(entity).attributes
+#        if 'LZW31-SN' in attributes['product_name']:
+#            node_ids.append(attributes['node_id'])
+#            
+#for node_id in node_ids:
+#    service_data = {'node_id': node_id, 'parameter': 16, 'size': 4, 'value': int(value)}
+#    hass.services.call('zwave', 'set_config_parameter', service_data, False)
